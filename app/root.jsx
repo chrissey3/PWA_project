a76ee9728255bc8ac9861cb47ab738f358570bce
Christoffer Lerager Rasmussen <chris1234ras.cr@gmail.com>
2022-04-26 12:48:46 +0200

    dfgh

--- app/root.jsx
+++ app/root.jsx
@@ -22,43 +22,14 @@ export const links = () => [
     rel: "stylesheet",
     href: styles,
   },
-  {
-  rel: "apple-touch-icon", 
-  sizes: "180x180", 
-  href: "/apple-touch-icon.png",
-  },
-  {
-    rel: "icon", 
-    type: "image/png", 
-    sizes: "32x32", 
-    href: "/favicon-32x32.png",
-  },
-  {
-    rel: "icon", 
-    type: "image/png", 
-    sizes: "16x16", 
-    href: "/favicon-16x16.png",
-  },
-  {
-    rel: "manifest", 
-    href: "/site.webmanifest",
-  },
-  {
-    rel: "mask-icon", 
-    href: "/safari-pinned-tab.svg", 
-    color: "#5bbad5",
-  }
 ];
 
 export function meta() {
-  return ({
+  return {
     charset: "utf-8",
-    title: "SSaver",
+    title: "Remix + MongoDB",
     viewport: "width=device-width,initial-scale=1",
-  },
-  {name: "msapplication-TileColor", content: "#da532c"},
-  {name: "theme-color", content: "#ffffff"}
-  );
+  };
 }
 
 export async function loader(){
--- app/routes/__snippet.jsx
+++ app/routes/__snippet.jsx
@@ -72,6 +72,7 @@ export async function loader({request}){
 }
 }
   return snippets;
+  
    
   
     
