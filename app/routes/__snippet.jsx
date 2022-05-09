733b852042d633bc659057f942d02d3c52f0cc10
Christoffer Lerager Rasmussen <chris1234ras.cr@gmail.com>
2022-05-09 13:50:11 +0200

    wasdzxfcgvhbjnkl

--- app/routes/__snippet.jsx
+++ app/routes/__snippet.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, Outlet, Form, useSubmit, useCatch } from "remix";
 import ListItem from "~/components/ListItem";
 import Searchbar from "~/components/Searchbar";
+import ErrorList from "~/components/ErrorList";
 
 
 import connectDb from "~/db/connectDb.server.js";
@@ -89,36 +90,7 @@ export function CatchBoundary(){
 
 export function ErrorBoundary({error}){
   return(
-    <>
-      
-    <div className="w-1/5  border-x-2 h-screen bg-white dark:bg-stone-900">
-      <div className="flex p-4 shadow">
-      <Searchbar />
-     
-     <Form method="get" action={url.pathname} onChange={handleChange}>
-     <select name="sort">
-       <option value="all">all</option>
-       <option value="asc">asc</option>
-       <option value="desc">desc</option>
-       <option value="newest">newest</option>
-       <option value="oldest">oldest</option>
-       <option value="fav">favorites</option>
-    </select>
-   
-     </Form>
-     </div>
-     <hr />
-     <ul>
-      
-     
-    </ul>
-    </div>
-    <div className="w-3/5 ">
-    <Outlet />
-
-    </div>
-    
-    </>
+   <ErrorList />
   )
  }
 
